f3f645b7a1749dd354867efe1be8c3220264a559
blizzard25 <esanders@protonmail.com>
2025-07-02 21:24:49 -0400

    Added slowapi rate limit of 1000 requests per hour

--- api/main.py
+++ api/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from api.routes import devices, activities, wallets, activity_types, healthz
 
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
+
+def get_api_key_or_ip(request: Request):
+    return request.headers.get("X-API-Key") or get_remote_address(request)
+
+# API will have a limit of 1000 requests per hour, limited by API key or IP address
+limiter = Limiter(key_func=get_api_key_or_ip, default_limits=["1000/hour"])
+
 app = FastAPI(title="Silvanus API")
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 app.include_router(devices.router, prefix="/devices", tags=["Devices"])
 app.include_router(activities.router, prefix="/activities", tags=["Activities"])
