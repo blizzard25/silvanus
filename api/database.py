5b4382ead4306cc50f1f1d8f96a6059459fc99bc
blizzard25 <esanders@protonmail.com>
2025-07-05 23:51:28 -0400

    added missing get_db method

--- api/database.py
+++ api/database.py
@@ -13,3 +13,10 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
