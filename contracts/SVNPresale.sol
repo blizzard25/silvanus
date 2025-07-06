c80404de72678ae37a16fcf39165c60978bc93ce
blizzard25 <esanders@protonmail.com>
2025-07-06 16:13:01 -0400

    updated SVNPresale

--- contracts/SVNPresale.sol
+++ contracts/SVNPresale.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SVNPresale is Ownable, ReentrancyGuard {
@@ -23,10 +23,11 @@ contract SVNPresale is Ownable, ReentrancyGuard {
     event TokensPurchased(address indexed buyer, uint256 amount, uint256 cost);
     event PresaleEnded(uint256 totalSold);
 
-    constructor(address _svnToken) {
-        require(_svnToken != address(0), "Invalid token address");
-        svnToken = IERC20(_svnToken);
-    }
+constructor(address _svnToken) Ownable(msg.sender) {
+    require(_svnToken != address(0), "Invalid token address");
+    svnToken = IERC20(_svnToken);
+}
+
 
     function buyTokens() external payable nonReentrant {
         require(!presaleEnded, "Presale has ended");
@@ -35,11 +36,7 @@ contract SVNPresale is Ownable, ReentrancyGuard {
         uint256 remaining = TOTAL_CAP - totalSold;
         require(remaining > 0, "All tokens sold");
 
-        uint256 tokensToBuy;
-        uint256 cost;
-
-        (tokensToBuy, cost) = _calculateTokenAmount(msg.value);
-
+        (uint256 tokensToBuy, uint256 cost) = _calculateTokenAmount(msg.value);
         require(tokensToBuy > 0, "Zero token purchase");
         require(tokensToBuy <= remaining, "Exceeds presale allocation");
 
@@ -47,47 +44,46 @@ contract SVNPresale is Ownable, ReentrancyGuard {
         totalSold += tokensToBuy;
 
         emit TokensPurchased(msg.sender, tokensToBuy, cost);
+
+        // Refund excess ETH if any
+        if (msg.value > cost) {
+            payable(msg.sender).transfer(msg.value - cost);
+        }
     }
 
     function _calculateTokenAmount(uint256 ethAmount) internal view returns (uint256 tokens, uint256 cost) {
         uint256 remaining = ethAmount;
         tokens = 0;
         cost = 0;
-        uint256 tierPrice;
 
         uint256 remainingTier1 = TIER1_CAP > totalSold ? TIER1_CAP - totalSold : 0;
         uint256 remainingTier2 = TIER2_CAP > totalSold ? TIER2_CAP - totalSold : 0;
         uint256 remainingTier3 = TOTAL_CAP - totalSold;
 
-        if (remainingTier1 > 0) {
-            tierPrice = TIER1_PRICE;
-            uint256 purchasable = (remaining * 1e18) / tierPrice;
+        if (remaining > 0 && remainingTier1 > 0) {
+            uint256 purchasable = (remaining * 1e18) / TIER1_PRICE;
             uint256 actual = purchasable > remainingTier1 ? remainingTier1 : purchasable;
-            uint256 tierCost = (actual * tierPrice) / 1e18;
+            uint256 tierCost = (actual * TIER1_PRICE) / 1e18;
             tokens += actual;
             cost += tierCost;
             remaining -= tierCost;
-            remainingTier1 -= actual;
         }
 
         if (remaining > 0 && remainingTier2 > 0) {
-            tierPrice = TIER2_PRICE;
-            uint256 purchasable = (remaining * 1e18) / tierPrice;
+            uint256 purchasable = (remaining * 1e18) / TIER2_PRICE;
             uint256 actual = purchasable > remainingTier2 ? remainingTier2 : purchasable;
-            uint256 tierCost = (actual * tierPrice) / 1e18;
+            uint256 tierCost = (actual * TIER2_PRICE) / 1e18;
             tokens += actual;
             cost += tierCost;
             remaining -= tierCost;
         }
 
         if (remaining > 0 && remainingTier3 > 0) {
-            tierPrice = TIER3_PRICE;
-            uint256 purchasable = (remaining * 1e18) / tierPrice;
+            uint256 purchasable = (remaining * 1e18) / TIER3_PRICE;
             uint256 actual = purchasable > remainingTier3 ? remainingTier3 : purchasable;
-            uint256 tierCost = (actual * tierPrice) / 1e18;
+            uint256 tierCost = (actual * TIER3_PRICE) / 1e18;
             tokens += actual;
             cost += tierCost;
-            remaining -= tierCost;
         }
     }
 
