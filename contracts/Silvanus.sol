07563a23d06f554180633440a86677e61ba359f9
blizzard25 <esanders@protonmail.com>
2025-07-09 13:12:49 -0400

    Updated token contract with grant allocation code replacing burn rate

--- contracts/Silvanus.sol
+++ contracts/Silvanus.sol
@@ -8,37 +8,44 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract Silvanus is Initializable, ERC20Upgradeable, Ownable2StepUpgradeable, UUPSUpgradeable {
     uint256 public burnRate;
+    address public grantWallet;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() {
-        _disableInitializers(); // disable initializer on implementation contract
+        _disableInitializers();
     }
 
     function initialize(uint256 initialSupply) public initializer {
         __ERC20_init("Silvanus", "SVN");
         __Ownable_init(msg.sender);
-
         __Ownable2Step_init();
         __UUPSUpgradeable_init();
 
-        burnRate = 0; // altering burn rate to 0 initially, will ugprade later
+        burnRate = 0; // Initially 0
         _mint(msg.sender, initialSupply);
     }
 
-    // Override ERC20's _update instead of _transfer (as of OZ 5.x)
+    // Override _update to split and redirect "burn"
     function _update(address from, address to, uint256 value) internal virtual override {
         uint256 burnAmount = (value * burnRate) / 10000;
         uint256 sendAmount = value - burnAmount;
 
         if (burnAmount > 0) {
-            super._update(from, address(0), burnAmount); // burn to zero address
+            require(grantWallet != address(0), "Grant wallet not set");
+            super._update(from, grantWallet, burnAmount); // Redirected to grant wallet
         }
+
         super._update(from, to, sendAmount);
     }
 
     function setBurnRate(uint256 _rate) external onlyOwner {
         require(_rate <= 10000, "Burn rate too high");
         burnRate = _rate;
+    }
+
+    function setGrantWallet(address _wallet) external onlyOwner {
+        require(_wallet != address(0), "Invalid address");
+        grantWallet = _wallet;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
